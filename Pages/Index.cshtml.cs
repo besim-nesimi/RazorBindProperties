de4cefa7d4d8a6237ebc4e3acc70f163c41d2a8a
bestronaut <nesimibesim@outlook.com>
2023-03-06 15:22:28 +0100

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorBindProperties.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+        public User NewUser { get; set; }
+
+        //public string? FirstName { get; set; }
+        //public string? LastName { get; set; }
+        //public int Age { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+        }
+
+        public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Email { get; set; }
+        }
+    }
+}
